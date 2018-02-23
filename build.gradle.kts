8d8fe256b2ec547a870509fe55969f7e5f6477d1
Peer Schönhusen <gonozalviii@hotmail.com>
2018-02-23 13:16:03 +0100

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.types.checker.captureFromArguments
 
 group = "com.github.gonozalviii"
-version = "0.1.1"
+version = "0.2"
 
 plugins {
     kotlin("jvm") version "1.2.21"
