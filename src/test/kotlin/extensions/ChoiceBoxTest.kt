74cd5b6dd1000624d434fda53c7d595bc83052a7
Ramon Victor <ramon.victor@t-online.de>
2018-03-01 14:48:02 +0100

    Add choicebox extensions test

---
+++ src/test/kotlin/extensions/ChoiceBoxTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2018 Peer Schoenhusen & Ramon Victor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package extensions
+
+import com.github.gonozalviii.kopperfx.extensions.addAll
+import com.github.gonozalviii.kopperfx.extensions.minusAssign
+import com.github.gonozalviii.kopperfx.extensions.plusAssign
+import com.github.gonozalviii.kopperfx.extensions.removeAll
+import javafx.embed.swing.JFXPanel
+import javafx.scene.control.Button
+import javafx.scene.control.ChoiceBox
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class ChoiceBoxTest {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun classSetup() {
+            JFXPanel() // init JavaFX
+        }
+    }
+
+    @Test
+    @DisplayName("plus assign on choiceBox adds button")
+    fun plusAssignAddsButton() {
+        val choiceBox = ChoiceBox<Button>()
+        val button = Button()
+
+        choiceBox += button
+
+        assertTrue(choiceBox.items.contains(button), "choicebox should contain button")
+    }
+
+    @Test
+    @DisplayName("minus assign on choiceBox removes button")
+    fun minusAssignRemovesButton() {
+        val choiceBox = ChoiceBox<Button>()
+        val button = Button()
+
+        choiceBox -= button
+
+        assertFalse(choiceBox.items.contains(button), "choicebox should not contain button")
+    }
+
+    @Test
+    @DisplayName("addAll on choiceBox adds all buttons")
+    fun addAllAddsAllButtons() {
+        val choiceBox = ChoiceBox<Button>()
+        val button1 = Button()
+        val button2 = Button()
+        val button3 = Button()
+
+        choiceBox.addAll(button1, button2, button3)
+
+        assertTrue(choiceBox.items.contains(button1), "choicebox should contain button1")
+        assertTrue(choiceBox.items.contains(button2), "choicebox should contain button2")
+        assertTrue(choiceBox.items.contains(button3), "choicebox should contain button3")
+    }
+
+    @Test
+    @DisplayName("removeAll on choiceBox removes all buttons")
+    fun removeAllRemovesAllButtons() {
+        val choiceBox = ChoiceBox<Button>()
+        val button1 = Button()
+        val button2 = Button()
+        val button3 = Button()
+        choiceBox.items.addAll(button1, button2, button3)
+
+        choiceBox.removeAll(button1, button2, button3)
+
+        assertFalse(choiceBox.items.contains(button1), "choicebox should not contain button1")
+        assertFalse(choiceBox.items.contains(button2), "choicebox should not contain button2")
+        assertFalse(choiceBox.items.contains(button3), "choicebox should not contain button3")
+    }
+
+}
