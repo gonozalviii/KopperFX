67a7002732fc4c42d40f5a6739afd1aae053538d
Ramon Victor <ramon.victor@t-online.de>
2018-03-13 09:43:48 +0100

    Add more tests for combobox extensions

--- src/test/kotlin/extensions/ComboBoxTest.kt
+++ src/test/kotlin/extensions/ComboBoxTest.kt
@@ -16,15 +16,11 @@
 
 package extensions
 
-import com.github.gonozalviii.kopperfx.extensions.addAll
-import com.github.gonozalviii.kopperfx.extensions.minusAssign
-import com.github.gonozalviii.kopperfx.extensions.plusAssign
-import com.github.gonozalviii.kopperfx.extensions.removeAll
+import com.github.gonozalviii.kopperfx.extensions.*
 import javafx.embed.swing.JFXPanel
 import javafx.scene.control.Button
 import javafx.scene.control.ComboBox
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -90,6 +86,49 @@ class ComboBoxTest {
         assertFalse(comboBox.items.contains(button1), "comboBox should not contain button1")
         assertFalse(comboBox.items.contains(button2), "comboBox should not contain button2")
         assertFalse(comboBox.items.contains(button3), "comboBox should not contain button3")
+    }
+
+
+    @Test
+    @DisplayName("addAt on comboBox adds button at index")
+    fun addAtAddsButtonAtIndex() {
+        val comboBox = ComboBox<Button>()
+        val button = Button()
+        val button1 = Button()
+        val button2 = Button()
+        val button3 = Button()
+        comboBox.items.addAll(button1, button2, button3)
+
+        comboBox.addAt(2, button)
+
+        assertEquals(button, comboBox.items[2], "comboBox should contain button at index")
+    }
+
+    @Test
+    @DisplayName("removeAt on comboBox removes button at index")
+    fun removeAtRemovesButtonAtIndex() {
+        val comboBox = ComboBox<Button>()
+        val button1 = Button()
+        val button2 = Button()
+        val button3 = Button()
+        comboBox.items.addAll(button1, button2, button3)
+
+        assertEquals(button2, comboBox.removeAt(1), "comboBox should not contain button at index")
+    }
+
+    @Test
+    @DisplayName("removeRange on comboBox removes buttons in range")
+    fun removeRangeRemovesButtonsInRange() {
+        val comboBox = ComboBox<Button>()
+        val button1 = Button()
+        val button2 = Button()
+        val button3 = Button()
+        comboBox.items.addAll(button1, button2, button3)
+
+        comboBox.removeRange(0, 2)
+
+        assertTrue(comboBox.items.size == 1, "comboBox should only contain one button")
+        assertEquals(button3, comboBox.items[0], "comboBox should only contain specific button")
     }
 
 }
