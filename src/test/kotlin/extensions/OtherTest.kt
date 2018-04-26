ed6f1e9676c71f8a323286ddabaedcb1e73027cb
Ramon Victor <ramon.victor@t-online.de>
2018-04-26 13:43:55 +0200

    Add test for others

--- src/test/kotlin/extensions/OtherTest.kt
+++ src/test/kotlin/extensions/OtherTest.kt
@@ -18,7 +18,15 @@ package extensions
 
 import base.AbstractJavaFxTestBase
 import com.github.gonozalviii.kopperfx.extensions.addExtensionFilter
+import com.github.gonozalviii.kopperfx.extensions.addIcons
+import com.github.gonozalviii.kopperfx.extensions.addStylesheet
+import com.github.gonozalviii.kopperfx.utility.fxThread
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.image.Image
 import javafx.stage.FileChooser
+import javafx.stage.Stage
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -35,6 +43,29 @@ class OtherTest : AbstractJavaFxTestBase() {
         val extensionFilter = fileChooser.extensionFilters[0]
         assertTrue(extensionFilter.description == "test", "filter should have test as description")
         assertTrue(extensionFilter.extensions[0] == "*.test", "filter should have *.test as extension")
+    }
+
+    @Test
+    @DisplayName("addIcons adds icon")
+    fun addIconsAddsIcon() {
+        fxThread {
+            val stage = Stage()
+            val image = Image("/KopperFX.png")
+
+            stage.addIcons("/KopperFX.png")
+
+            assertEquals(image.url, stage.icons[0].url, "stage should contain specific icon")
+        }
+    }
+
+    @Test
+    @DisplayName("addStylsheet adds stylesheet")
+    fun addStylesheetAddsStylesheet() {
+        val scene = Scene(Button())
+
+        scene.addStylesheet("css")
+
+        assertEquals("css", scene.stylesheets[0], "scene should contain specific stylesheet")
     }
 
 }
