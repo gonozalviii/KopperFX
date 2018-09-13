dafab08bd74f9cb213815ab37905b6169ebc66f9
Peer Schönhusen <gonozalviii@hotmail.com>
2018-09-13 19:50:31 +0200

    Add Maven like console output when running tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
     mavenCentral()
 }
 
+apply(from = "testreport.gradle.kts")
+
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
---
+++ testreport.gradle.kts
@@ -0,0 +1,25 @@
+tasks {
+    withType<Test> {
+
+        beforeSuite(closureOf<TestDescriptor> {
+            if(this.parent != null && this.className != null) {
+                println("Running ${this.className}")
+            }
+        })
+
+        afterSuite(KotlinClosure2<TestDescriptor, TestResult, Any>({
+                desc, result->
+            if(desc.parent != null && desc.className != null) {
+                println("Tests run: ${result.testCount}, " +
+                        "Failures: ${result.failedTestCount}, " +
+                        "Skipped: ${result.skippedTestCount}, " +
+                        "Time elapsed: ${(result.endTime - result.startTime) / 1000f} sec")
+            }
+            desc.parent ?:
+            println("\nResults:\n\n" +
+                    "Tests run: ${result.testCount}, " +
+                    "Failures: ${result.failedTestCount}, " +
+                    "Skipped: ${result.skippedTestCount}")
+        }))
+    }
+}
